6e7f18cb1b54fb9460baaaa18465ba3d6015ee0a
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-03-13 22:28:56 -0400

    base de datos vacia agregada

---
+++ test/evalua.sql
@@ -0,0 +1,278 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE `Alumno` (
+  `matricula` int NOT NULL,
+  `nombre` varchar(60) DEFAULT NULL,
+  `apellido_materno` varchar(50) DEFAULT NULL,
+  `apellido_paterno` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+CREATE TABLE `Carrera` (
+  `id_carrera` int NOT NULL,
+  `nombre_carrera` varchar(60) DEFAULT NULL,
+  `nombre_corto` varchar(50) DEFAULT NULL,
+  `correo_institucional` varchar(120) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+
+CREATE TABLE `CuestionarioAlumnoDocente` (
+  `id_cuestionario_ad` int NOT NULL,
+  `descripcion` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+--
+-- Estructura de tabla para la tabla `Curso`
+--
+
+CREATE TABLE `Curso` (
+  `id_curso` int NOT NULL,
+  `id_periodo` int NOT NULL,
+  `id_materia` int NOT NULL,
+  `id_grupo` int NOT NULL,
+  `id_docente` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Curso_has_Alumno`
+--
+
+CREATE TABLE `Curso_has_Alumno` (
+  `id_curso` int NOT NULL,
+  `matricula` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+--
+-- Estructura de tabla para la tabla `Docente`
+--
+
+CREATE TABLE `Docente` (
+  `id_docente` int NOT NULL,
+  `nombre` varchar(60) DEFAULT NULL,
+  `apellido_materno` varchar(50) DEFAULT NULL,
+  `apellido_paterno` varchar(50) DEFAULT NULL,
+  `correo` varchar(120) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+--
+-- Estructura de tabla para la tabla `Encuesta`
+--
+
+CREATE TABLE `Encuesta` (
+  `id_encuesta` int NOT NULL,
+  `id_curso` int NOT NULL,
+  `matricula_alumno` int NOT NULL,
+  `id_cuestionario_ad` int NOT NULL,
+  `estado` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+--
+-- Estructura de tabla para la tabla `Grupo`
+--
+
+CREATE TABLE `Grupo` (
+  `id_grupo` int NOT NULL,
+  `clave_grupo` varchar(20) DEFAULT NULL,
+  `id_carrera` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+--
+-- Estructura de tabla para la tabla `Materia`
+--
+
+CREATE TABLE `Materia` (
+  `id_materia` int NOT NULL AUTO_INCREMENT,
+  `nombre_materia` varchar(60) DEFAULT NULL,
+  `nombre_corto_materia` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+--
+-- Estructura de tabla para la tabla `Periodo`
+--
+
+CREATE TABLE `Periodo` (
+  `id_periodo` int NOT NULL,
+  `Estado` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+--
+-- Estructura de tabla para la tabla `Pregunta`
+--
+
+CREATE TABLE `Pregunta` (
+  `id_pregunta` int NOT NULL AUTO_INCREMENT,
+  `id_cuestionario_ad` int NOT NULL,
+  `pregunta` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+--
+-- Estructura de tabla para la tabla `Respuesta`
+--
+
+CREATE TABLE `Respuesta` (
+  `id_encuesta` int NOT NULL,
+  `id_pregunta` int NOT NULL,
+  `id_cuestionario_ad` int NOT NULL,
+  `puntuacion` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+
+
+--
+-- Indices de la tabla `Alumno`
+--
+ALTER TABLE `Alumno`
+  ADD PRIMARY KEY (`matricula`);
+
+--
+-- Indices de la tabla `Carrera`
+--
+ALTER TABLE `Carrera`
+  ADD PRIMARY KEY (`id_carrera`);
+
+--
+-- Indices de la tabla `CuestionarioAlumnoDocente`
+--
+ALTER TABLE `CuestionarioAlumnoDocente`
+  ADD PRIMARY KEY (`id_cuestionario_ad`);
+
+--
+-- Indices de la tabla `Curso`
+--
+ALTER TABLE `Curso`
+  ADD PRIMARY KEY (`id_curso`,`id_periodo`,`id_materia`,`id_grupo`,`id_docente`),
+  ADD KEY `fk_Curso_Materia2_idx` (`id_materia`),
+  ADD KEY `fk_Curso_Grupo2_idx` (`id_grupo`),
+  ADD KEY `fk_Curso_Docente2_idx` (`id_docente`),
+  ADD KEY `fk_Curso_Periodo1_idx` (`id_periodo`);
+
+--
+-- Indices de la tabla `Curso_has_Alumno`
+--
+ALTER TABLE `Curso_has_Alumno`
+  ADD PRIMARY KEY (`id_curso`,`matricula`),
+  ADD KEY `fk_Curso_has_Alumno_Alumno1_idx` (`matricula`),
+  ADD KEY `fk_Curso_has_Alumno_Curso1_idx` (`id_curso`);
+
+--
+-- Indices de la tabla `Docente`
+--
+ALTER TABLE `Docente`
+  ADD PRIMARY KEY (`id_docente`);
+
+--
+-- Indices de la tabla `Encuesta`
+--
+ALTER TABLE `Encuesta`
+  ADD PRIMARY KEY (`id_encuesta`,`id_curso`,`matricula_alumno`,`id_cuestionario_ad`),
+  ADD KEY `fk_Encuesta_Curso2_idx` (`id_curso`),
+  ADD KEY `fk_Encuesta_Alumno2_idx` (`matricula_alumno`),
+  ADD KEY `fk_Encuesta_CuestionarioAlumnoDocente1_idx` (`id_cuestionario_ad`);
+
+--
+-- Indices de la tabla `Grupo`
+--
+ALTER TABLE `Grupo`
+  ADD PRIMARY KEY (`id_grupo`,`id_carrera`),
+  ADD KEY `fk_Grupo_Carrera2_idx` (`id_carrera`);
+
+--
+-- Indices de la tabla `Materia`
+--
+ALTER TABLE `Materia`
+  ADD PRIMARY KEY (`id_materia`);
+
+--
+-- Indices de la tabla `Periodo`
+--
+ALTER TABLE `Periodo`
+  ADD PRIMARY KEY (`id_periodo`);
+
+--
+-- Indices de la tabla `Pregunta`
+--
+ALTER TABLE `Pregunta`
+  ADD PRIMARY KEY (`id_pregunta`,`id_cuestionario_ad`),
+  ADD KEY `fk_Pregunta_CuestionarioAlumnoDocente1_idx` (`id_cuestionario_ad`);
+
+--
+-- Indices de la tabla `Respuesta`
+--
+ALTER TABLE `Respuesta`
+  ADD KEY `fk_Respuesta_CuestionarioAlumnoDocente1_idx` (`id_cuestionario_ad`),
+  ADD KEY `fk_Respuesta_Pregunta2` (`id_pregunta`),
+  ADD KEY `fk_Respuesta_Encuesta3` (`id_encuesta`);
+
+
+--
+-- Filtros para la tabla `Curso`
+--
+ALTER TABLE `Curso`
+  ADD CONSTRAINT `fk_Curso_Docente2` FOREIGN KEY (`id_docente`) REFERENCES `Docente` (`id_docente`),
+  ADD CONSTRAINT `fk_Curso_Grupo2` FOREIGN KEY (`id_grupo`) REFERENCES `Grupo` (`id_grupo`),
+  ADD CONSTRAINT `fk_Curso_Materia2` FOREIGN KEY (`id_materia`) REFERENCES `Materia` (`id_materia`),
+  ADD CONSTRAINT `fk_Curso_Periodo1` FOREIGN KEY (`id_periodo`) REFERENCES `Periodo` (`id_periodo`);
+
+--
+-- Filtros para la tabla `Curso_has_Alumno`
+--
+ALTER TABLE `Curso_has_Alumno`
+  ADD CONSTRAINT `fk_Curso_has_Alumno_Alumno1` FOREIGN KEY (`matricula`) REFERENCES `Alumno` (`matricula`),
+  ADD CONSTRAINT `fk_Curso_has_Alumno_Curso1` FOREIGN KEY (`id_curso`) REFERENCES `Curso` (`id_curso`);
+
+--
+-- Filtros para la tabla `Encuesta`
+--
+ALTER TABLE `Encuesta`
+  ADD CONSTRAINT `fk_Encuesta_Alumno2` FOREIGN KEY (`matricula_alumno`) REFERENCES `Alumno` (`matricula`),
+  ADD CONSTRAINT `fk_Encuesta_CuestionarioAlumnoDocente1` FOREIGN KEY (`id_cuestionario_ad`) REFERENCES `CuestionarioAlumnoDocente` (`id_cuestionario_ad`),
+  ADD CONSTRAINT `fk_Encuesta_Curso2` FOREIGN KEY (`id_curso`) REFERENCES `Curso` (`id_curso`);
+
+--
+-- Filtros para la tabla `Grupo`
+--
+ALTER TABLE `Grupo`
+  ADD CONSTRAINT `fk_Grupo_Carrera2` FOREIGN KEY (`id_carrera`) REFERENCES `Carrera` (`id_carrera`);
+
+--
+-- Filtros para la tabla `Pregunta`
+--
+ALTER TABLE `Pregunta`
+  ADD CONSTRAINT `fk_Pregunta_CuestionarioAlumnoDocente1` FOREIGN KEY (`id_cuestionario_ad`) REFERENCES `CuestionarioAlumnoDocente` (`id_cuestionario_ad`);
+
+--
+-- Filtros para la tabla `Respuesta`
+--
+ALTER TABLE `Respuesta`
+  ADD CONSTRAINT `fk_Respuesta_CuestionarioAlumnoDocente1` FOREIGN KEY (`id_cuestionario_ad`) REFERENCES `CuestionarioAlumnoDocente` (`id_cuestionario_ad`),
+  ADD CONSTRAINT `fk_Respuesta_Encuesta3` FOREIGN KEY (`id_encuesta`) REFERENCES `Encuesta` (`id_encuesta`),
+  ADD CONSTRAINT `fk_Respuesta_Pregunta2` FOREIGN KEY (`id_pregunta`) REFERENCES `Pregunta` (`id_pregunta`);
+COMMIT;
+
